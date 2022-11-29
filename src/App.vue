1b41ecc8b7ab1589873e402f9648816abc8ec5f5
SabinaValeriia <valeriia.sabina@onix-systems.com>
2022-11-29 20:32:59 +0200

    Add component Header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   #app
+    <Header></Header>
     <PostForm></PostForm>
     .post(v-for="post in validPosts"  :key="post.id")
       h2 {{post.title}}
@@ -9,6 +10,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import PostForm from './components/PostForm.vue'
+import Header from './components/Header.vue'
 import { mapGetters, mapActions, mapMutations} from 'vuex'
 
 export default Vue.extend({
@@ -22,7 +24,7 @@ export default Vue.extend({
     
   },
   components: {
-    PostForm,
+    PostForm, Header
   },
   async mounted() {
     this.fetchPosts(4);
@@ -31,13 +33,57 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+*, *:before, *:after{
+    box-sizing: border-box;
+}
+
+html, body, div, span, object, iframe, figure, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, code, em, img, small, strike, strong, sub, sup, tt, b, u, i, ol, ul, li, fieldset, form, label, table, caption, tbody, tfoot, thead, tr, th, td, main, canvas, embed, footer, header, nav, section, video{
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    text-size-adjust: none;
+}
+
+footer, header, nav, section, main{
+    display: block;
+}
+
+body{
+    line-height: 1;
+}
+
+ol, ul{
+    list-style: none;
+}
+
+blockquote, q{
+    quotes: none;
+}
+
+blockquote:before, blockquote:after, q:before, q:after{
+    content: '';
+    content: none;
+}
+
+table{
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+input{
+    -webkit-appearance: none;
+    border-radius: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #081b2e;
-  background: rgb(170, 170, 179);
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+    .container 
+        .header 
+            img(src="../assets/img/logo.png", alt="")
+            .header__box
+                img.header__img(src="../assets/img/globe-solid.png", alt="")
+                p.header__text 日本語
+        .header__box
+            .header__box_check
+                img.header__img(src="../assets/img/checkin.png", alt="")
+                p.header__text Not selected
+            .header__box_check
+                img.header__img(src="../assets/img/checkout.png", alt="")
+                p.header__text Not selected
+            .header__box_check
+                img.header__img(src="../assets/img/profile.png", alt="")
+                p.header__text Not selected
+            button.header__box_button Check Availability
+</template>
+
+
+<style lang="scss">
+.container{
+    padding: 0 20px;
+}
+.header{
+    margin: 12px 0px 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &__text{
+        font-family: 'MPLUS1p-Regular';
+        font-size: 16px;
+        line-height: 24px;
+    }
+    &__img{
+        width: 19px;
+        height: 19px;
+        margin-right: 8px;
+    }
+    &__box{
+        display: flex;
+        align-items: center;
+        &_check{
+            display: flex;
+            align-items: center;
+        }
+        &_button {
+            background: #FD7900;
+            border-radius: 4px;
+            border: none;
+            width: 194px;
+            height: 48px;
+        }
+    }
+}
+</style>
