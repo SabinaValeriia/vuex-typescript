169b429c6344bf43ca1aab5e26d8118d89c8d797
SabinaValeriia <valeriia.sabina@onix-systems.com>
2022-11-29 11:48:29 +0200

    Add modules users

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
     .post(v-for="post in validPosts"  :key="post.id")
       h2 {{post.title}}
       p {{post.body}}
+
+    .user(v-for="user in validUsers" :key="user.id")
+      h2 {{user.name}}
+      p {{user.username}}
+     
+
 </template>
 
 <script lang="ts">
@@ -14,16 +20,17 @@ import { mapGetters, mapActions} from 'vuex'
 export default Vue.extend({
   name: 'App',
   computed: {
-    ...mapGetters(['validPosts']),
+    ...mapGetters(['validPosts', 'validUsers']),
    
   },
-  methods: {...mapActions(["fetchPosts"])},
+  methods: {...mapActions(["fetchPosts", "fetchUsers"])},
   components: {
     PostForm,
   },
   async mounted() {
     this.fetchPosts(4);
     console.log(111);
+    this.fetchUsers();
   }
 });
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import Vuex, { StoreOptions } from 'vuex'
 import post from './modules/post'
+import user from './modules/user'
 
 Vue.use(Vuex)
 
 let store =  new  Vuex.Store ({
  
   modules: {
-    post
+    post, 
+    user
   }
 });
 
